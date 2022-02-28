2eda8fda659f3d50500450893272015fc3289508
unknown <tompsherman@gmail.com>
2022-02-28 17:17:36 -0500

    end day 1

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # cs50
 
 This repository follows Harvard's CS-50 course. Should be fun.
+
+c return type, name of function, and arguments in that order, if none then void
---
+++ calc2.c
@@ -0,0 +1,18 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+
+// prompt for x
+    int x = get_int("x: ");
+
+    // prompt for y
+    int y = get_int("y: ");
+
+    // divide x by y
+
+    float z = (float) x / (float) y
+
+    printf("%.2f\n", z);
+}
